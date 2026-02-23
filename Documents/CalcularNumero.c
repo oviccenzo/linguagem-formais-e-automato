fb560ab4a5c70e9ca3b1fd7f147f356d033cca36
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-02-23 19:37:20 -0300

    Delete Documents directory

--- Documents/CalcularNumero.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-	int A, B, Soma;
-	
-	printf("Digite um numero inteiro: ");
-	scanf("%d",&A)
-}
--- Documents/swith.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Robert L Resende on 01/12/24.
-//
-
-#include "swith.h"
